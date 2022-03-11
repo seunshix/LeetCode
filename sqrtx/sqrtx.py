d9523d0267c1addf8d94e50dceb468e94b0e033e
Adebimpe Adeniran <86207070+seunshix@users.noreply.github.com>
2022-03-11 13:31:42 -0800

    Time: 57 ms, Memory: 13.8 MB - LeetHub

--- sqrtx/sqrtx.py
+++ sqrtx/sqrtx.py
@@ -1,18 +1,20 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+    
         if x < 2:
             return x
         
-        left, right = 0, x/2
+        left, right = 2, x // 2
         
-        while right >= left:
-            pivot = left + (right - left)//2
-            a = pivot * pivot
-            if a > x:
-                right = pivot - 1
-            elif a < x:
+        while left <= right:
+            pivot = left + (right - left) // 2
+            num = pivot * pivot
+            if num > x:
+                right = pivot -1
+            elif num < x:
                 left = pivot + 1
             else:
-                return int(pivot)
-        return int(right)
+                return pivot
+            
+        return right
                 
