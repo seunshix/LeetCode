b18b28c9dead4020099f0854d2812ee72d63d3a7
Adebimpe Adeniran <86207070+seunshix@users.noreply.github.com>
2022-05-01 16:59:30 -0700

    Time: 63 ms (36.78%), Space: 14.1 MB (92.03%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,67 +1,39 @@
+# Formula: If a sorted array is shifted, if you take the middle, always one side will be sorted. Take the recursion according to that rule.
+
+# 1- take the middle and compare with target, if matches return.
+# 2- if middle is bigger than left side, it means left is sorted & Binary search can be applied on left side.
+# 2a- if [left] < target < [middle] then do recursion with left, middle - 1 (right)
+# 2b- left side is sorted, but target not in here, search on right side {middle + 1 (left), right} => Here one is NOT claiming that right side is sorted. 
+# We are NOT applying binary search here per-se. We are just reducing our search space since we are sure that element is NOT in the part 2a. 
+# The next iteration in this method would try to figure out which part of the subarray needs to be looked into.
+# 3- if middle is less than right side, it means right is sorted
+# 3a- if [middle] < target < [right] then do recursion with middle + 1 (left), right
+# 3b- right side is sorted, but target not in here, search on left side i.e. {left, middle -1 (right)} => Here one is NOT claiming that left side is sorted. 
+# We are NOT applying binary search here per-se. We are just reducing our search space since we are sure that element is NOT in the part 3a. 
+# The next iteration in this method would try to figure out which part of the subarray needs to be looked into.
+
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        start, end = 0, len(nums) - 1
-        while start <= end:
-            mid = start + (end - start) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] >= nums[start]:
-                if target >= nums[start] and target < nums[mid]:
-                    end = mid - 1
-                else:
-                    start = mid + 1
-            else:
-                if target <= nums[end] and target > nums[mid]:
-                    start = mid + 1
-                else:
-                    end = mid - 1
-        return -1
-
-
-
-# class Solution:
-#     def search(self, nums: List[int], target: int) -> int:
-#         def find_rotate_index(left, right):
-#             if nums[left] < nums[right]:
-#                 return 0
-#             while left <= right:
-#                 pivot = left + (right - left)//2
-#                 if nums[pivot] > nums[pivot + 1]:
-#                     return pivot + 1
-#                 elif nums[pivot] < nums[left]:
-#                     right = pivot - 1
-#                 else:
-#                     left = pivot + 1
-                    
-#         def search(left, right):
-#             while left <= right:
-#                 pivot = (left + right) // 2
-#                 if nums[pivot] == target:
-#                     return pivot
-#                 else:
-#                     if target < nums[pivot]:
-#                         right = pivot - 1
-#                     else:
-#                         left = pivot + 1
-#             return -1
-    
-#         n = len(nums)
-
-#         if n == 1:
-#             return 0 if nums[0] == target else -1
-
-#         rotate_index = find_rotate_index(0, n - 1)
-
-#         # if target is the smallest element
-#         if nums[rotate_index] == target:
-#             return rotate_index
-#         # if array is not rotated, search in the entire array
-#         if rotate_index == 0:
-#             return search(0, n - 1)
-#         if target < nums[0]:
-#             # search on the right side
-#             return search(rotate_index, n - 1)
-#         # search on the left side
-#         return search(0, rotate_index)
-  
+    def search(self, nums: List[int], target: int, start = None, end = None) -> int:
+        if len(nums) == 1 and nums[0] != target: return -1
         
+        if start is None:
+            start = 0
+            end = len(nums) - 1
+        mid = (start + end) // 2
+        
+        if nums[mid] == target:
+            return mid
+        
+        if nums[start]  <= target <= nums[mid]:
+            return self.search(nums, target, start, mid)
+        
+        if nums[mid + 1] <= target <= nums[end]:
+            return self.search(nums, target, mid + 1, end)
+        
+        if nums[start] > nums[mid]:
+            return self.search(nums, target, start, mid)
+        
+        if nums[mid + 1] > nums[end]:
+            return self.search(nums, target, mid + 1, end)
+        
+        return -1
