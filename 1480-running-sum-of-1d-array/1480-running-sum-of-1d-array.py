00901c9eb409e69cbe2b5e12db0edf9bee081417
Adebimpe Adeniran <86207070+seunshix@users.noreply.github.com>
2022-07-12 15:26:36 -0700

    Time: 61 ms (54.48%), Space: 14.2 MB (26.01%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -2,10 +2,11 @@ class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         '''
         1. iterate through the array
-        2. at each index, calculate the sums of index i to index n-1
+        2. at each index, calculate the sums of index i to index i-1
         3. return answer in result
         4. this solution can be done in-place
-        
+        TC : O(N) ... iterating through the array
+        SC : O(1) ... no extra space used 
         '''
         n = len(nums) 
         for i in range(1, n):
