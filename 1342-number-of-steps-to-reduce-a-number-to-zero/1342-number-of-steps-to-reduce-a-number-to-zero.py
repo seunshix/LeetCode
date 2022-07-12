32c7c3a4b6e6d0c96acad715af003f68b983545d
Adebimpe Adeniran <86207070+seunshix@users.noreply.github.com>
2022-07-12 16:32:05 -0700

    Time: 34 ms (85.67%), Space: 13.8 MB (52.31%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        '''
+        1. we have to keep running count of steps
+        2. if the number is divisible by 2, divide it by 2
+        3. if number is odd, subract 1
+        '''
+        steps = 0
+        while num > 0:
+            if num % 2 == 0:
+                num = num/2
+                steps +=1
+            if num % 2 == 1:
+                num = num - 1
+                steps +=1
+        return steps
+            
