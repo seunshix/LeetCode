149f05a051079c1edd87f9efc36ee865c5ff0850
Adebimpe Adeniran <86207070+seunshix@users.noreply.github.com>
2022-07-14 16:28:38 -0700

    Time: 193 ms (80.64%), Space: 15.5 MB (65.09%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -1,19 +1,12 @@
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        
-        1. it is a two pointer problem
-        2. the left pointer and right pointer should start from the beginning
-        3. we then iterate through the array using the right pointer. if the element at right pointer is non-zero, we swap elements at the left and right pointer
-        4. return the array
-        """
-        left, right = 0, 0
-        while right < len(nums):
-            if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-            right+=1
+class Solution(object):
+    def moveZeroes(self, nums):
+        i = count = 0
+        while count < len(nums):
+            
+            if nums[i] == 0: 
+                nums.append(nums.pop(i))
+            else: 
+                i += 1
+            count += 1
         return nums
-        
         
