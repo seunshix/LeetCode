75f3825ed1f345760a509800b31b494506d21ecd
Seun Adeniran <86207070+seunshix@users.noreply.github.com>
2022-08-18 14:32:11 -0700

    Time: 434 ms (24.01%), Space: 21.2 MB (74.99%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,10 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        '''
+        
         n = len(nums)
-        print(n)
         answer = [1] * n
+        prefix = 1
+        postfix = 1
+        
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+            
+        for i in range(n - 1, -1, -1):
+            answer[i] *= postfix
+            postfix*= nums[i]
+            
+        return answer
         
         '''
         # O(n) time complexity and space complexity
@@ -31,4 +42,4 @@ class Solution:
         
         print(prefix)
         print(postfix)
-        
+        '''
