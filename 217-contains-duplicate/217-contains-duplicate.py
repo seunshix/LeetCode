50f52875f23a6abb1645873dbc205617b0ce22a8
Seun Adeniran <86207070+seunshix@users.noreply.github.com>
2022-08-18 13:07:46 -0700

    Time: 676 ms (51.87%), Space: 25.9 MB (92.60%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,5 +1,17 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        hset = set()
+        for i in nums:
+            if i in hset:
+                return True
+            hset.add(i)
+        return False
+                
+        
+        '''
+        
+        O(nlogn) solution
         n = len(nums)
         nums.sort()
         if n < 2:
@@ -8,5 +20,9 @@ class Solution:
             if nums[i-1] == nums[i]:
                 return True
         return False
+        
+        
+        '''
+
                 
         
