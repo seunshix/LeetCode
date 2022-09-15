155ddbc0f4e91d25077e40ff9c45d4a026434e1a
Seun Adeniran <86207070+seunshix@users.noreply.github.com>
2022-09-15 14:29:51 -0700

    Time: 4269 ms (5.01%), Space: 25.3 MB (7.04%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,64 @@
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float("inf")
-        max_profit = 0
-        for i in range(len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            elif prices[i] - min_price > max_profit:
-                max_profit = prices[i] - min_price
+        n = len(prices)
         
-        return max_profit
+        if n < 2:
+            return 0
+        lo, hi = 0, n-1
+        mid = lo + (hi - lo)//2
+        
+        left_prices = prices[0:mid+1]
+        right_prices = prices[mid+1: ]
+        
+        left_profit = self.maxProfit(left_prices)
+        right_profit = self.maxProfit(right_prices)
+        
+        left_min = min(left_prices)
+        right_max = max(right_prices)
+        
+        combine_profits = right_max - left_min
+        
+        return max(left_profit, right_profit, combine_profits)
+        
+        '''
+        
+        O(n^2) solution TLE, Brute Force
+        def maxProfit(self, prices: List[int]) -> int:
+        larger_difference = 0
+        n = len(prices)
+        for i in range(n):
+            for j in range(i+1, n):
+                if prices[j] - prices[i] > larger_difference:
+                    larger_difference = prices[j] - prices[i]
+        return larger_difference
+        
+        
+        '''
+        
+        
+        '''
+        Divide and conquer approach
+        def maxProfit(self, prices: List[int]) -> int:
+       
+        n = len(prices)
+        if n < 2:
+            return 0
+        
+        mid = n//2
+        
+        left_prices = prices[: mid+1]
+        right_prices = prices[mid-1: ]
+        
+        left_profit = self.maxProfit(left_prices)
+        right_profit = self.maxProfit(right_prices)
+        
+        left_min = min(left_prices)
+        right_max = max(right_prices)
+        
+        combine_profits = right_max - left_min
+        
+        return max(left_profit, right_profit, combine_profits)
+        
+        '''
+        
