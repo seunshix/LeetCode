7756c6243a9c03e1b3aaac5cd64b6ceee6af56f8
Seun Adeniran <86207070+seunshix@users.noreply.github.com>
2022-10-03 20:03:03 -0700

    Time: 359 ms (15.24%), Space: 20.8 MB (33.81%) - LeetHub

--- 359-logger-rate-limiter/359-logger-rate-limiter.py
+++ 359-logger-rate-limiter/359-logger-rate-limiter.py
@@ -1,29 +1,14 @@
-from collections import deque
 class Logger:
 
     def __init__(self):
-        self.messageSet = set()
-        self.messageQueue = deque()
+        self.messageDict = {}
         
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        
-        while self.messageQueue:
-            msg, ts = self.messageQueue[0]
-            print("A", msg, ts)
-            if timestamp - ts >= 10:
-                self.messageQueue.popleft()
-                self.messageSet.remove(msg)
-            else:
-                break
-        
-        print("B", self.messageQueue)
-        print("C", self.messageSet)   
-        if message not in self.messageSet:
-            self.messageSet.add(message)
-            self.messageQueue.append((message, timestamp))
+        if message not in self.messageDict or timestamp - self.messageDict[message] >= 10:
+            self.messageDict[message] = timestamp
             return True
-        else:
+        else: 
             return False
         
 
